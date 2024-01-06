10247928cfa69c10c7b4790b61eb4b7f72a50261
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-07 00:19:29 +0100

    linting rules disabled

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
@@ -10,7 +11,15 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={30} />
+      <pointLight intensity={25} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
