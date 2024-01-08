c18e755323939104de4b7647e2166092df696e37
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-08 22:20:56 +0100

    projects cards added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]"></div>
+      </Tilt>
+    </motion.div>
+  );
+};
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Portfolio</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
 
-export default Works
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Below are some examples of my own personal projects that I have worked
+          on over the last 3 years. With each project I was always trying to
+          learn a specific technology or library. Here I have included a brief
+          description, the GitHub repo and, if it is live, a link to see the
+          project itself.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, idx) => (
+          <ProjectCard key={`project-${idx}`} index={idx} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, '');
