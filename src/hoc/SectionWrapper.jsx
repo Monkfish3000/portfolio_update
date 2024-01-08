1bb22dbabbb859bab1f5f397dc6f6f8b8c966339
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-08 16:16:58 +0100

    scroll to id added to HOC

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,6 +13,9 @@ const SectionWrapper = (Component, idName) => {
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
+        <span id={idName} className="hash-span">
+          &nbsp
+        </span>
         <Component />
       </motion.section>
     );
