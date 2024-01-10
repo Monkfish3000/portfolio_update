0bc4be6ec909e323a0089949b6b0d4a7f3ad88cc
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-10 14:36:25 +0100

    testimonial card added

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
+
+const FeedBackCard = ({
+  name,
+  index,
+  testimonial,
+  designation,
+  company,
+  image,
+}) => {
+  return (
+    <motion.div>
+      <p>{testimonial}</p>
+    </motion.div>
+  );
+};
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[200px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={`${styles.sectionSubText}`}>What others say</p>
+          <h2 className={`${styles.sectionHeadText}`}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((test, idx) => (
+          <FeedBackCard key={test.name} index={idx} {...test} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Feedbacks
+export default Feedbacks;
