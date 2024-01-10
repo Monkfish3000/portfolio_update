622122d9b79f4866a8fc26eeeaacc6da1b28d126
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-10 14:48:23 +0100

    testimonials cards styled

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -14,8 +14,30 @@ const FeedBackCard = ({
   image,
 }) => {
   return (
-    <motion.div>
-      <p>{testimonial}</p>
+    <motion.div
+      className="bg-black-200 p-10 rounded-3xl xs:w-[320px w-full]"
+      variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+    >
+      <p className="text-white font-black text-[48px]">"</p>
+      <div className="mt-1">
+        <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+
+        <div className="mt-7 flex justify-between items-center gap-1">
+          <div className="flex-1 flex flex-col">
+            <p className="text-white font-medium text-[16px]">
+              <span className="blue-text-gradient">@</span> {name}
+            </p>
+            <p className="mt-1 text-secondary text-[12px]">
+              {designation} of {name}
+            </p>
+          </div>
+          <img
+            className="w-10 h-10 rounded-full object-cover"
+            src={image}
+            alt={`feedback-by-${name}`}
+          />
+        </div>
+      </div>
     </motion.div>
   );
 };
@@ -24,7 +46,7 @@ const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div
-        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[200px]`}
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
       >
         <motion.div variants={textVariant()}>
           <p className={`${styles.sectionSubText}`}>What others say</p>
@@ -40,4 +62,4 @@ const Feedbacks = () => {
   );
 };
 
-export default Feedbacks;
+export default SectionWrapper(Feedbacks, '');
